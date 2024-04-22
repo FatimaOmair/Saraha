5c6562a23a3b6266ff5b1148ccf613301e8bd620
FatimaOmair <fatimaemer2002@gmail.com>
2024-04-22 10:06:12 +0300

    add generalFileds

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,6 +1,11 @@
+import joi from "joi";
+
 const dataMethods=['body', 'query', 'params','headers']
 
-
+export const generalFileds={
+  email:joi.string().min(5).max(30).email().required(),
+  password:joi.string().min(8).max(20).required(),
+}
 
 
 const validation = (scheme) => {
