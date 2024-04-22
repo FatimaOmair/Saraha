ae854390e26f64f09d43f417984a66dedacdd84d
FatimaOmair <fatimaemer2002@gmail.com>
2024-04-22 10:07:23 +0300

    make auth validation dynamic

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -1,12 +1,13 @@
 import joi from 'joi';
+import { generalFileds } from '../../middleware/validation.js';
 export const signUpSchema={
     body:joi.object({
         userName:joi.string().alphanum().min(3).max(20).required().messages({
             "string.empty":"User name is required",
             "any.required":"User name is required"
         }),
-        email:joi.string().email().required(),
-        password:joi.string().min(8).max(20).required(),
+        email:generalFileds.email,
+        password:generalFileds.password,
         cPassword:joi.valid(joi.ref('password')).required(),
         age:joi.number().min(20).positive().integer(),
         gender:joi.string().alphanum().valid('male', 'female').required()
@@ -22,5 +23,5 @@ export const signUpSchema={
 export const signInSchema=
 {body:joi.object({
     email:joi.string().email().required(),
-    password:joi.string().min(8).max(20).required(),
+    password:generalFileds.password,
 })}
