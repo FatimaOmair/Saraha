7ef137a3e5e9873edad5e1f80e3ae4bebbb81829
FatimaOmair <fatimaemer2002@gmail.com>
2024-05-05 16:57:06 +0300

    ass app.get success at the begining

--- src/modules/app.router.js
+++ src/modules/app.router.js
@@ -4,7 +4,11 @@ import authRouter from './auth/auth.router.js'
 import userRouter from './user/user.router.js'
 const initApp=(app,express)=>{
     app.use(express.json());
+    
     connectDB();
+    app.get('/',(req,res)=>{
+        return res.status(200).json({message:"success"})
+     })
     app.use('/message',messageRouter)
     app.use('/auth',authRouter)
     app.use('/user',userRouter)
